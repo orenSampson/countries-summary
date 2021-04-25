5e3322587bd389ccfbb87bed1806651b451ca170
oren sampson <oren.sampson@gmail.com>
2021-04-25 17:35:59 +0300

    refactoring

--- app.js
+++ app.js
@@ -2,65 +2,90 @@ const mongoose = require("mongoose");
 const axios = require("axios");
 
 const CountriesSummary = require("./models/countriesSummary");
+const AdminCountry = require("./models/adminCountry");
 const { MONGODB_URI, COVID_BASE_URL } = require("./constants");
 
 exports.app = async () => {
-    try {
-        await mongoose.connect(MONGODB_URI, {
-            useNewUrlParser: true,
-            useCreateIndex: true,
-            useUnifiedTopology: true,
-            useFindAndModify: false,
-        });
-    } catch (error) {
-        return console.log(error);
-    }
-
-    let countriesSummary;
-    try {
-        countriesSummary = await axios.get(COVID_BASE_URL + "/summary");
-        countriesSummary = countriesSummary.data.Countries;
-    } catch (error) {
-        return console.log(error);
-    }
-
-    if (!countriesSummary) {
-        return console.log("error retrieving countries summary");
-    }
-
-    countriesSummary = countriesSummary.map((country) => ({
-        countryName: country.Country,
-        slug: country.Slug,
-        totalConfirmed: country.TotalConfirmed,
-        newConfirmed: country.NewConfirmed,
-        totalDeaths: country.TotalDeaths,
-        totalRecovered: country.TotalRecovered,
-    }));
-
-    const countriesObj = {
-        countries: countriesSummary,
-    };
-
-    CountriesSummary.estimatedDocumentCount(async function (err, count) {
-        if (err) {
-            console.log(err);
-        } else {
-            if (count === 0) {
-                const countriesSummaryCollection = new CountriesSummary(
-                    countriesObj
-                );
-                try {
-                    await countriesSummaryCollection.save();
-                } catch (error) {
-                    console.log("error :>> ", error);
-                }
-            } else {
-                try {
-                    await CountriesSummary.findOneAndUpdate({}, countriesObj);
-                } catch (error) {
-                    console.log("error :>> ", error);
-                }
-            }
-        }
+  try {
+    await mongoose.connect(MONGODB_URI, {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false,
     });
+  } catch (error) {
+    return console.log(error);
+  }
+
+  let countriesSummary;
+  try {
+    countriesSummary = await axios.get(COVID_BASE_URL + "/summary");
+    countriesSummary = countriesSummary.data.Countries;
+  } catch (error) {
+    return console.log(error);
+  }
+
+  if (!countriesSummary) {
+    return console.log("error retrieving countries summary");
+  }
+
+  countriesSummary = countriesSummary.map((country) => ({
+    countryName: country.Country,
+    slug: country.Slug,
+    totalConfirmed: country.TotalConfirmed,
+    newConfirmed: country.NewConfirmed,
+    totalDeaths: country.TotalDeaths,
+    totalRecovered: country.TotalRecovered,
+  }));
+
+  const countriesObj = {
+    countries: countriesSummary,
+  };
+
+  CountriesSummary.estimatedDocumentCount(async function (err, count) {
+    if (err) {
+      console.log(err);
+    } else {
+      if (count === 0) {
+        const countriesSummaryCollection = new CountriesSummary(countriesObj);
+        try {
+          await countriesSummaryCollection.save();
+        } catch (error) {
+          console.log("error :>> ", error);
+        }
+      } else {
+        try {
+          await CountriesSummary.findOneAndUpdate({}, countriesObj);
+        } catch (error) {
+          console.log("error :>> ", error);
+        }
+      }
+    }
+  });
+
+  // let adminSelectedCountries;
+  // try {
+  //   adminSelectedCountries = await AdminCountry.find({
+  //     isSelected: true,
+  //   }).lean();
+  // } catch (error) {
+  //   return console.log("error retrieving admin countries");
+  // }
+  // if (!adminSelectedCountries) {
+  //   return console.log("error retrieving admin countries");
+  // }
+
+  // console.log("adminSelectedCountries :>> ", adminSelectedCountries);
+
+  // const adminSelectedCountriesSlugs = new Set(
+  //   adminSelectedCountries.map(({ slug }) => slug)
+  // );
+
+  // console.log("countriesSummary before :>> ", countriesSummary);
+
+  // countriesSummary = countriesSummary.filter(({ slug }) =>
+  //   adminSelectedCountriesSlugs.has(slug)
+  // );
+
+  // console.log("countriesSummary after :>> ", countriesSummary);
 };
